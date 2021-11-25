76eec3f7d70f346519f0f436def1de2afe3a8500
Haavi <javierortincervera@gmail.com>
2021-11-25 21:54:30 +0200

    First arrangements for Sparta

--- config/adminAddress.js
+++ config/adminAddress.js
@@ -1,3 +1,3 @@
-const AdminAddresses = ["0xb7bc6d2666e73f8cd143a929db5404e2fc03ea89"];
+const AdminAddresses = ["0x3C7aEF7d580D82A679CC3B287934b894d99043Db"];
 
 module.exports = AdminAddresses;
--- constants/factory_abi.js
+++ constants/factory_abi.js
@@ -1,21 +1,19 @@
 const CollectionFactoryContract = {
-  MAINNET_721_PRIVATE: '0xa4fDb09e1796730bfBA8a352074F0dd65D400Dd4', //FantomNFTFactoryPrivate
-  MAINNET_721_PUBLIC: '0xCC7A2eC7A8A0564518fD3D2ca0Df8B2137626144', //FantomNFTFactory
-  TESTNET_721_PRIVATE: '0xC3d390628D8d7080197cA6a79a7ABa0430229C37', //FantomNFTFactoryPrivate
-  TESTNET_721_PUBLIC: '0xA7dacD023F5d5e827cEC42255EA859A1544269Af', //FantomNFTFactory
-  MAINNET_1155_PRIVATE: '0x736Eae40AdFf88570b92378c97a0D11b44E1C953', //FantomArtFactoryPrivate
-  MAINNET_1155_PUBLIC: '0x520DaB621f93F59d3557174280AB1B6d4FB8c956', //FantomArtFactory
-  TESTNET_1155_PRIVATE: '0x8637DBB197768e8Ad0E5cc98f738B64C10452F72', //FantomArtFactoryPrivate
-  TESTNET_1155_PUBLIC: '0x864Acb6d06E24486730138245da3A612b74c1Ddf', //FantomArtFactory
-  ABI: [
-    {
-      inputs: [{ internalType: 'address', name: '', type: 'address' }],
-      name: 'exists',
-      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-      stateMutability: 'view',
-      type: 'function'
-    }
-  ]
+    MAINNET_721_PRIVATE: '0xdb97189CB4be936919594Ba5b6e8DA0c6649313c', //FantomNFTFactoryPrivate
+    MAINNET_721_PUBLIC: '0xc389b01A6b56e9Ba23e8F201a257168a51f14762', //FantomNFTFactory
+    TESTNET_721_PRIVATE: '0xdb97189CB4be936919594Ba5b6e8DA0c6649313c', //FantomNFTFactoryPrivate
+    TESTNET_721_PUBLIC: '0xc389b01A6b56e9Ba23e8F201a257168a51f14762', //FantomNFTFactory
+    MAINNET_1155_PRIVATE: '', //FantomArtFactoryPrivate
+    MAINNET_1155_PUBLIC: '', //FantomArtFactory
+    TESTNET_1155_PRIVATE: '', //FantomArtFactoryPrivate
+    TESTNET_1155_PUBLIC: '', //FantomArtFactory
+    ABI: [{
+        inputs: [{ internalType: 'address', name: '', type: 'address' }],
+        name: 'exists',
+        outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+        stateMutability: 'view',
+        type: 'function'
+    }]
 };
 
 module.exports = CollectionFactoryContract;
