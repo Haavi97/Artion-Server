3df5891c2cdb4ff89a2f720e98bc8fa62628610a
Haavi <javierortincervera@gmail.com>
2021-12-21 08:37:06 +0200

    Minor changes for authentication and launching

--- apis/middleware/auth.tracker.js
+++ apis/middleware/auth.tracker.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const requestIP = require("request-ip");
+const Logger = require("../../services/logger");
 
 const tracker_ip1 = process.env.TRACKER_IP1;
 const tracker_ip2 = process.env.TRACKER_IP2;
@@ -9,12 +10,14 @@ const tracker_ip5 = process.env.TRACKER_IP5;
 
 const service_auth = (req, res, next) => {
   let request_ip = requestIP.getClientIp(req);
+  Logger.debug("RequestedIP: " + request_ip);
 
   if (
     request_ip == tracker_ip1 ||
     request_ip == tracker_ip2 ||
     request_ip == tracker_ip3 ||
-    request_ip == tracker_ip4
+    request_ip == tracker_ip4 ||
+    request_ip == tracker_ip5
   ) {
     next();
   } else
